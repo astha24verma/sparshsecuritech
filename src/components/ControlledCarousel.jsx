bfb05581694f46785b310e6519ba09d4d03dfe29
Astha Verma <astha24verma@gmail.com>
2024-02-28 00:41:15 +0530

    Added ControlledCarousal and NavbarSetupApp components

---
+++ src/components/ControlledCarousel.jsx
@@ -0,0 +1,58 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function ControlledCarousel() {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={'assets/img/homepage-banner-1.png'} />
+                    <Carousel.Caption>
+                        {/* <h3>First Demo </h3> */}
+                    </Carousel.Caption>
+                </Carousel.Item  >
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={'assets/img/homepage-banner-bus-solution-2.png'} />
+                    <Carousel.Caption>
+                        {/* <h3>Second Demo</h3> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={'assets/img/homepage-banner-mobile-app-3.png'} />
+                    <Carousel.Caption>
+                        {/* <h3>Third Demo</h3> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={'assets/img/homepage-banner-home-security-4.png'} />
+                    <Carousel.Caption>
+                        {/* <h3>Forth Demo</h3> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={'assets/img/homepage-product-5.png'} />
+                    <Carousel.Caption>
+                        {/* <h3>Fifth Demo</h3> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+            </Carousel>
+        </div>
+    )
+}
+
+
+export default ControlledCarousel;
