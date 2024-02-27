d1430245a997e16cd829a6ccbe001b712709642a
Astha Verma <astha24verma@gmail.com>
2024-02-28 02:42:26 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarSetupApp from './components/NavbarSetupApp';
 import ControlledCarousel from './components/ControlledCarousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Solutions from './components/Solutions';
+import Products from './components/Products';
+import FooterSetupApp from './components/FooterSetupApp';
 
 function App() {
   return (
     <div className="App">
         <NavbarSetupApp/>
         <ControlledCarousel/>
+        <Solutions/>
+        <Products/>
+        <FooterSetupApp/>
     </div>
   );
 }
